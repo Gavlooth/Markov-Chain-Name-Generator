2c664a185f04e479d6de2a5dae8f4d58e8edbded
Christos Chatzifountas <heefoo@outlook.com>
2022-06-20 14:52:36 +0300

    Add functions to manipulate sylables

--- src/main.lisp
+++ src/main.lisp
@@ -13,6 +13,25 @@
 
 (defvar  sylables '())
 
+
+(defclass sylable ()
+ ((sylable
+   :initarg :sylable
+   :accessor sylable)
+  (associated-sylables
+   :initform nil
+   :accessor lisper)))
+
+
+(defun get-sylables (word)
+  (iter (for idx index-of-string word)
+    (with max = (length word))
+    (unless (=< max ( + 2 idx))
+      (print idx)
+      (collect (map 'string (lambda (x) (char word (+ x  idx))) '( 0 1 2))))))
+
+(get-sylables "afkslfdflkdafdkafkjafkj")
+
 (iter (for  word in data)
    (iter (for idx index-of-string word)
          (with max = (length word))
@@ -20,4 +39,33 @@
          (terminate))
      (setq sylables (cons (map 'string (lambda (x) (char word (+ x  idx))) '( 0 1 2)) sylables))))
 
-(print sylables)
+(setq sylables (remove-duplicates sylables))
+
+
+(uiop:split-string "fdasfsdf" :separator "d")
+
+(defun remove-empty-strings (s-list)
+ (iter (for s in  s-list)
+     (unless (string= s "")
+       (collect s))))
+
+(defun get-probability (sylable)
+ (iter (for  word in data)
+       (with after-sylable-body = (uiop:split-string word :separator sylable))
+   (unless (string= word (car sylable)))))
+
+
+(second (uiop:split-string "ii.iii":separator "."))
+
+
+(with-open-file (stream "markov-matrix.txt" :direction :output :if-does-not-exist :create)
+ (let (uniq-sylables (remove-duplicates sylables))
+
+  (iter (for sylable in uniq-sylables)
+   (format stream  "~a " sylable))
+
+  (iter (for sylable in uniq-sylables)
+   (format stream  "~a ~a" sylable #\Newline))))
+
+;(open "/some/file/name.txt" :direction :output :if-exists :supersede)
+;(asdf:load-system "markov-chain-namegen")
